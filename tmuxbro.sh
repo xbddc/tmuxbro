31f9d595799124e4a5fe77845db2227434268332
xbddc <xbddc.ho@gmail.com>
2013-06-12 22:51:45 +0800

    update to v0.2, use tmux panes instead of tmux windows

--- tmuxbro.sh
+++ tmuxbro.sh
@@ -1,40 +1,63 @@
 #!/bin/sh
 
+show_menu=1
 do_multicast=0
-tagged_window=""
+tagged_pane=""
+curr_pane="%1"
+next_pane=""
+prev_pane=""
+pane_list=""
+
+toggle_menu () {
+  show_menu=$(($(($show_menu+1))%2))
+}
 
 func_menu () {
   clear
-  echo "==[ tmuxbro v0.1 ]=============="
-  echo "[1;32m[F2][m: go previous window"
-  echo "[1;32m[F3][m: go next window"
-  echo "[1;32m[F4][m: remove current window"
-  echo "[1;32m[F5][m: tag/untag current window"
-  echo "[1;32m[F6][m: tag/untag all windows"
-  echo "[1;32m[F7][m: toggle multicast"
-  echo "[1;32m[F8][m: add host"
-  echo "[1;32m[F12][m: quit program"
-  echo "================================"
+  if [ $show_menu = 1 ]; then
+    echo "========[ tmuxbro v0.2 ]========"
+    echo "[1;32m[F2][m: go previous window"
+    echo "[1;32m[F3][m: go next window"
+    echo "[1;32m[F4][m: remove current window"
+    echo "[1;32m[F5][m: tag/untag current window"
+    echo "[1;32m[F6][m: tag/untag all windows"
+    echo "[1;32m[F7][m: toggle multicast"
+    echo "[1;32m[F8][m: add host"
+    echo "[1;32m[F9][m: show/hide menu"
+    echo "[1;32m[F12][m: quit program"
+    echo "================================"
+  else
+    echo "[1;32m[F9][m: show/hide menu"
+  fi
 
-  curr=`tmux list-windows -t ssh-$$ | grep \(active\) | cut -d: -f1`
-  list=`tmux list-windows -t ssh-$$ | grep -Eo '.+\[[0-9x]+\]'`
-  list=`echo "$list" | sed "s/^$curr: \(.*\)$/$curr: [1;42m\1[m/g"`
-
-  for x in $tagged_window; do
-    if [ $do_multicast = 1 ]; then
-      list=`echo "$list" | sed "s/^$x: \(.*\)$/$x: \1[1;37;41m*[m/g"`
-    else
-      list=`echo "$list" | sed "s/^$x: \(.*\)$/$x: \1[1;37m*[m/g"`
-    fi
-  done
+  get_pane_list
+  list=`tmux list-panes -a -t ssh-$sid -F "#{pane_id}  #{pane_start_command} " | grep -v '^%0 ' | sort`
+  list=`echo "$list" | sed "s:^$curr_pane \(.*\)$:$curr_pane [1;42m\\1[m:g"`
+  if [ $do_multicast = 0 ]; then
+    for x in $tagged_pane; do
+      list=`echo "$list" | sed "s:^$x \(.*\)$:$x \\1 [1;37m*[m:g"`
+    done
+  else
+    for x in $tagged_pane; do
+      list=`echo "$list" | sed "s:^$x \(.*\)$:$x \\1 [1;37;41m*[m:g"`
+    done
+  fi
 
   OLD_IFS=$IFS; IFS=\n
-  echo $list
+  echo $list | sed "s:^%\([0-9]*\) :\[\\1\] :g"
   IFS=$OLD_IFS
 
   if [ $do_multicast = 1 ]; then
-    echo -n "[1;41m!!! MULTICAST MODE !!![m"
+    echo -n "\n[1;41m!!! MULTICAST MODE !!![m"
   fi
+}
+
+get_pane_list () {
+  pane_list=`tmux list-panes -a -t ssh-$sid -F "#{pane_id}" | grep -v '^%0$' | sort`
+  OLD_IFS=$IFS; IFS=\n
+  prev_pane=`echo $pane_list | grep -B1 "$curr_pane" | head -n1`
+  next_pane=`echo $pane_list | grep -A1 "$curr_pane" | tail -n1`
+  IFS=$OLD_IFS
 }
 
 get_keystroke () {
@@ -44,92 +67,74 @@ get_keystroke () {
   stty $old_stty_settings
 }
 
-check_session () {
-  tmux has-session -t ssh-$$ 2>/dev/null
-  if [ $? != 0 ]; then
-    echo "session not found, quit."
-    exit 3
-  fi
-
-  list=`tmux list-window -t ssh-$$ | cut -d: -f1`
-  new_tagged_window=""
-  for x in $tagged_window; do
-    for y in $list; do
-      if [ "$x" = "$y" ]; then
-        new_tagged_window="$x $new_tagged_window"
-        break
-      fi
-    done
-  done
-  tagged_window=$new_tagged_window
-}
-
-attach_session () {
-  echo -n "attaching session ... "
-  while [ 1 ]; do
-    tmux has-session -t ssh-$$ 2>/dev/null
-    if [ $? = 0 ]; then
-      break
-    else
-      tmux new-session -d -s ssh-$$
-      sleep 1
-      tmux set-option -t ssh-$$ remain-on-exit on 1>/dev/null
-    fi
-  done
-  echo "done."
-}
-
 kill_session () {
-  echo -n "\ndisconnect from each host? (y/n/c) "
+  echo "\nclose all remote connections?"
+  echo -n "([y]es/[n]o/[c]ancel) "
   n=`get_keystroke`
   if [ "$n" != "n" ] && [ "$n" != "y" ]; then
     return
   elif [ "$n" = "y" ]; then
     echo -n "killing session ... "
     while [ 1 ]; do
-      tmux has-session -t ssh-$$ 2>/dev/null
+      tmux has-session -t ssh-$sid 2>/dev/null
       if [ $? != 0 ]; then
         break
       else
-        tmux kill-session -t ssh-$$
+        tmux kill-session -t ssh-$sid
       fi
     done
+  else
+    tmux detach
   fi
+
   echo "bye."
   exit 0
 }
 
-create_window () {
-  check_session
-  tmux new-window -t ssh-$$ -n "$1" "$1"
+join_pane () {
+  tmux join-pane -s $1 -t %0 -h -d
+  adj_width=$(( `tmux list-pane -a -F "#{pane_width} #{pane_id}" | grep %0 | cut -d\  -f1`-32 ))
+  tmux resize-pane -t $curr_pane -L $adj_width
 }
 
-remove_window_0 () {
-  check_session
-  tmux kill-window -t ssh-$$:0
+create_window () {
+  tmux new-window -d -t ssh-$sid -n "$1" "$1"
 }
 
 prev_window () {
-  check_session
-  tmux select-window -t ssh-$$ -p
+  if [ "$curr_pane" != "$prev_pane" ]; then
+    tmux break-pane -t $curr_pane -d
+    curr_pane=$prev_pane
+    join_pane $curr_pane
+  fi
 }
 
 next_window () {
-  check_session
-  tmux select-window -t ssh-$$ -n
+  if [ "$curr_pane" != "$next_pane" ]; then
+    tmux break-pane -t $curr_pane -d
+    curr_pane=$next_pane
+    join_pane $curr_pane
+  fi
 }
 
 del_window () {
-  check_session
   echo -n "\nremove this window? (y/n) "
   n=`get_keystroke`
   if [ "$n" = "y" ]; then
-    tmux kill-window -t ssh-$$
+    tmux kill-pane -t $curr_pane
+    if [ "$curr_pane" != "$prev_pane" ]; then
+      curr_pane=$prev_pane
+      join_pane $curr_pane
+    elif [ "$curr_pane" != "$next_pane" ]; then
+      curr_pane=$next_pane
+      join_pane $curr_pane
+    else
+      curr_pane=""
+    fi
   fi
 }
 
 add_window () {
-  check_session
   while [ 1 ]; do
     read -p "add a host: " host
     if [ "$host" != "" ]; then
@@ -142,89 +147,79 @@ add_window () {
 }
 
 multicast () {
-  check_session
   if [ $do_multicast = 1 ]; then
-    echo "$tagged_window" | tr " " "\n" | xargs -I{} tmux send-keys -t ssh-$$:{} "$@"
+   echo "$tagged_pane" | tr " " "\n" | xargs -I{} tmux send-keys -t {} "$@"
   else
-    tmux send-keys -t ssh-$$ "$@"
+    tmux send-keys -t $curr_pane "$@"
   fi
 }
 
 tag_untag_all_windows () {
-  check_session
-  list=`tmux list-windows -t ssh-$$ | cut -d: -f1 | sort | tr "\n" " "`
-  tagged_window=`echo "$tagged_window" | tr " " "\n" | grep -v ^$ | sort | tr "\n" " "`
-  if [ "$list" = "$tagged_window" ]; then
-    tagged_window=""
+  tagged_pane=`echo "$tagged_pane" | tr " " "\n" | grep -v ^$ | sort | tr "\n" " "`
+  pane_list_tmp=`echo "$pane_list" | tr "\n" " "`
+  if [ "$pane_list_tmp" = "$tagged_pane" ]; then
+    tagged_pane=""
   else
-    tagged_window=$list
+    tagged_pane=$pane_list_tmp
   fi
 }
 
 tag_untag_window () {
-  check_session
-  new_tagged_window=""
+  new_tagged_pane=""
   found=0
-  curr=`tmux list-windows -t ssh-$$ | grep \(active\) | cut -d: -f1`
 
-  for x in $tagged_window; do
-    if [ "$x" = "$curr" ]; then
+  for x in $tagged_pane; do
+    if [ "$x" = "$curr_pane" ]; then
       found=1
     else
-      new_tagged_window="$x $new_tagged_window"
+      new_tagged_pane="$x $new_tagged_pane"
     fi
   done
 
   if [ $found = 1 ]; then
-    tagged_window=$new_tagged_window
+    tagged_pane=$new_tagged_pane
   else
-    tagged_window="$curr $new_tagged_window"
+    tagged_pane="$curr_pane $new_tagged_pane"
   fi
 }
 
 toggle_multicast () {
   do_multicast=$(($(($do_multicast+1))%2))
   if [ $do_multicast = 1 ]; then
-    tmux set-window-option -t ssh-$$ status-bg red 1>/dev/null
+    tmux set-window-option -t ssh-$sid status-bg red 1>/dev/null
   else
-    tmux set-window-option -t ssh-$$ status-bg green 1>/dev/null
+    tmux set-window-option -t ssh-$sid status-bg green 1>/dev/null
   fi
 }
-
-if [ "$1" = "-h" ]; then
-  echo "usage: $0 [/path/to/host_list]"
-  exit 0
-fi
 
 if [ ! -f `which tmux` ]; then
   echo "$0: tmux not found"
   exit 1
 fi
 
-attach_session
+#phase1
+if [ "$1" != "tmuxbro_phase2" ]; then
+  tmux new-session -s ssh-$$ "sh $0 tmuxbro_phase2 $$ $1"
+  sleep 1
+  tmux set-option -t ssh-$$ remain-on-exit on 1>/dev/null
+  exit
+fi
 
-if [ ! -z $1 ] && [ -f $1 ]; then
-  list=`cat $1`
-  for host in $list; do 
+#phase2
+sid=$2
+host_file=$3
+
+if [ ! -z $host_file ] && [ -f $host_file ]; then
+  for host in `cat $host_file`; do 
     create_window "ssh $host"
   done
 else
   add_window force
 fi
 
-remove_window_0
+tmux select-window -t 0
+join_pane $curr_pane
 func_menu
-echo "focus on this terminal to pass cmds"
-
-if [ -x "`which x-terminal-emulator`" ]; then
-  x-terminal-emulator -e tmux attach-session -t ssh-$$ 2>/dev/null &
-elif [ -x "`which gnome-terminal`" ]; then
-  gnome-terminal -e "tmux attach-session -t ssh-$$" 2>/dev/null &
-elif [ -x "`which xterm`" ]; then
-  xterm -e "tmux attach-session -t ssh-$$" 2>/dev/null &
-else
-  echo "please open a new terminal and run 'tmux attach-session -t ssh-$$' to get hosts output"
-fi
 
 while [ 1 ]; do
   m=`get_keystroke`
@@ -249,6 +244,9 @@ while [ 1 ]; do
     ;;
     \[19~) #F8
       add_window
+    ;;
+    \[20~) #F9
+      toggle_menu
     ;;
     \[24~) #F12
       kill_session
